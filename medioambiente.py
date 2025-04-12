220da174e82a00bae9446256adaf484b582c62d8
matiasando <matiasandovalandrade@gmail.com>
2025-04-12 00:27:16 -0500

    Create medioambiente.py
    pa tu medio ambiente cuidalo bro :)

---
+++ medioambiente.py
@@ -0,0 +1,113 @@
+
+# This example requires the 'members' and 'message_content' privileged intents to function.
+
+import discord
+import os
+import requests
+from discord.ext import commands
+
+import random
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+    
+@bot.command()
+async def datos(ctx):
+    images = random.choice(os.listdir("Imagenes"))
+    
+    with open(f'Imagenes/{images}','rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    
+    lista_datos = ["Más de 7 millones de personas mueren prematuramente debido a la contaminación del aire cada año.",
+             "Unos 1,5 millones de aves, peces, ballenas y tortugas mueren al año por desechos plásticos en el mar.",
+             " La exposición a sustancias químicas cancerígenas presentes en el aire, el agua y los alimentos contaminados puede aumentar el riesgo de desarrollar diversos tipos de cáncer."
+             "https://www.nationalgeographicla.com/medio-ambiente/2022/08/cuales-son-los-principales-tipos-de-contaminacion-ambiental"]
+    await ctx.send("Sabias que:")
+    await ctx.send(random.choice(lista_datos))
+    
+    await ctx.send(file=picture)
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+        
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+
+@cool.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the bot is cool.')
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+bot.run("TU TOKEN AQUI ")
